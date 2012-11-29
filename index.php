554676dd699bc29d6738da20c1d96fdae73523d7
Micheal Mueller <msquared86@gmail.com>
2012-11-28 21:25:12 -0500

    initial commit
    thisis the initial commit it is working justneed to add some features

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: student
+ * Date: 11/28/12
+ * Time: 4:30 PM
+ * To change this template use File | Settings | File Templates.
+ */
+require_once 'ageVerificationVerification.php';
+$age = new AgeVerification();
+
+$age -> getAge(11,29,1986);
