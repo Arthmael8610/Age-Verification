4f3a5b2b1d139edcdf9a7ca0e2f016bc15a309e0
Micheal Mueller <msquared86@gmail.com>
2012-11-29 00:05:52 -0500

    initial Commit from IDE, working, added features planned: config file for changing age limit, redirect URL, admin panel for setting options

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
  * Time: 4:30 PM
  * To change this template use File | Settings | File Templates.
  */
-require_once 'ageVerificationVerification.php';
+require_once 'ageVerification.php';
 $age = new AgeVerification();
 
 $age -> getAge(11,29,1986);
